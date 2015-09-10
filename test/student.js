e8dbddb9f3087cf31fc59d567a592db5f0c8a32d
Matthew <matthewmarcos94@gmail.com>
2015-09-10 13:50:17 +0800

    Added fail test cases to moccha

--- test/student.js
+++ test/student.js
@@ -12,25 +12,69 @@ describe('student', function() {
 					if(err) {
 						throw err;
 					}
-					// res.body.should.be.an.instanceOf(Array).and.have.lengthOf(7);
+					res.body.should.be.an.instanceOf(Array);
+					// res.body.should.have.keys();
+
 					done();
 				});
 		});
 	});
-	describe('find', function() {
-		it('should retrieve all student records', function(done) {
+	describe('insert', function() {
+		it('should insert successfully', function(done) {
 			request(url)
 				.post('/students')
 				.send({
 					'studno': '2013-36122',
-					'name': "<script> alert('Hi'); </script>"
+					'name': 'Joseph Matthew R. Marcos'
 				})
 				.expect(200)
 				.end(function(err, res) {
 					if(err) {
 						throw err;
 					}
+					console.log("STUFF");
+					console.log(res.body);
 					res.should.be.json
+					// res.body.should.have.keys(['affectedRows']);
+					res.body.should.have.property('affectedRows', 1);
+					done();
+				});
+		});
+		it('should not insert successfully', function(done) {
+			request(url)
+				.post('/students')
+				.send({
+					'studno': '2013-36122'
+				})
+				.expect(200)
+				.end(function(err, res) {
+					if(err) {
+						throw err;
+					}
+					console.log("STUFF");
+					console.log(res.body);
+					res.should.be.json
+					// res.body.should.have.keys(['affectedRows']);
+					res.body.should.have.property('affectedRows', 1);
+					done();
+				});
+		});
+		it('should not insert successfully', function(done) {
+			request(url)
+				.post('/students')
+				.send({
+					'name': 'Joseph Matthew R. Marcos'
+				})
+				.expect(200)
+				.end(function(err, res) {
+					if(err) {
+						throw err;
+					}
+					console.log("STUFF");
+					console.log(res.body);
+					res.should.be.json
+					// res.body.should.have.keys(['affectedRows']);
+					res.body.should.have.property('affectedRows', 1);
 					done();
 				});
 		});
